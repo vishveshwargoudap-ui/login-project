b2e322756ed23ac0c0cd9d770077d9e1085c92e3
vishveshwargoudap-ui <vishveshwargoudap@gmail.com>
2026-02-21 15:24:16 +0530

    role

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    role=db.Column(db.String(50), nullable=False, default='user')   
 
     def __init__(self, name, email, password):
         self.name = name
