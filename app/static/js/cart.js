78315703e67ce3b147486d6092f26662818e8261
vishveshwargoudap-ui <vishveshwargoudap@gmail.com>
2026-02-23 23:41:12 +0530

    place order error

--- app/static/js/cart.js
+++ app/static/js/cart.js
@@ -1,52 +1,56 @@
-function addToCart(button){
+function addToCart(button) {
     console.log("add to cart clicked");
-const id=button.dataset.id;
-const name=button.dataset.name;
-const price=parseFloat(button.dataset.price);
-const image=button.dataset.image;
 
-fetch("/add_to_cart",{
-    method:"POST",
-    headers:{
-        "Content-Type":"application/json"
-},
-    body:JSON.stringify({
-        id:id,
-        name:name,
-        price:price,
-        image:image
-    })
-}).then(response=>response.json())
-.then(data=>{
-    console.log(data);
-    alert(data.message);
-})
-.catch(error=>{
-    console.error("Error:",error);
-    alert("An error occurred while adding to cart.");
-});
-}
-function placeOrder(){
-    console.log("place order clicked");
-    fetch("/place-order",{
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json"
+    const id = button.dataset.id;
+    const name = button.dataset.name;
+    const price = parseFloat(button.dataset.price);
+    const image = button.dataset.image;
+
+    fetch("/add_to_cart", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
         },
-        body:JSON.stringify({
-            payment_mode:"cash"
+        body: JSON.stringify({
+            id: id,
+            name: name,
+            price: price,
+            image: image
         })
     })
-    .then(response=>response.json())
-    .then(data=>{
-        if(data.ok){
-            window.location.href=data.redirect_url;
-        } else{
-            alert(data.messsage);
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+    })
+    .catch(error => {
+        console.error(error);
+        alert("Error adding to cart");
+    });
+}
+
+function placeOrder() {
+    console.log("Place order clicked");
+
+    fetch("/place-order", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            payment_mode: "cash"
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if (data.ok) {
+            window.location.href = data.redirect_url;
+        } else {
+            alert(data.message);
         }
     })
-    .catch(error=>{
+    .catch(error => {
         console.error(error);
-        alert("An error occurred while placing the order.");
+        alert("Error placing order");
     });
 }
