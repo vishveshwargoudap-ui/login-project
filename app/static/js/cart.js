c81e753e5cd65e86389323ddea7b7140571d2dd5
vishveshwargoudap-ui <vishveshwargoudap@gmail.com>
2026-02-23 12:12:25 +0530

    cart error

--- app/static/js/cart.js
+++ app/static/js/cart.js
@@ -1,13 +1,25 @@
-function addToCart(id, name, price, image) {
-    const cartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
-    const existing = cartItems.find(item => item.id === id);
+function addToCart(button){
+const id=button.dataset.id;
+const name=button.dataset.name;
+const price=button.dataset.price;
+const image=button.dataset.image;
 
-    if (existing) {
-        existing.qty += 1;
-    } else {
-        cartItems.push({ id, name, price, image, qty: 1 });
-    }
-
-    localStorage.setItem("cartItems", JSON.stringify(cartItems));
-    alert(name + " added to cart");
+fetch("/add_to_cart",{
+    method:"POST",
+    headers:{
+        "Content-Type":"application/json"
+},
+    body:JSON.stringify({
+        id:id,
+        name:name,
+        price:price,
+        image:image
+    })
+}).then(response=>response.json())
+.then(data=>{
+    alert(data.message);
+})
+.catch(error=>{
+    console.error("Error:",error);
+});
 }
