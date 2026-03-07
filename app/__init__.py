ae133c74b79751d5a360e7a60b8d48a02bcb3977
vishveshwargoudap-ui <vishveshwargoudap@gmail.com>
2026-03-07 12:11:30 +0530

    security

--- app/__init__.py
+++ app/__init__.py
@@ -16,12 +16,13 @@ def create_app():
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "default_secret_key")
     app.config["SESSION_COOKIE_SECURE"]=True
     app.config["SESSION_COOKIE_SAMESITE"]="Lax"
-    app.config['Mail_SERVER']='smpt.gmail.com'
-    app.config['Mail_PORT']=587
+    app.config['MAIL_SERVER']='smpt.gmail.com'
+    app.config['MAIL_PORT']=587
     app.config['MAIL_USE_TLS']=True
-    app.config['Mail_USERNAME']='vishveshwargoudap@gmail.com'
-    app.config['MAIL_PASSWORD']='nmeg lktn fyyj sewu'
+    app.config['MAIL_USERNAME']='vishveshwargoudap@gmail.com'
+    app.config['MAIL_PASSWORD']=os.environ.get("MAIL_PASSWORD")
     mail = Mail(app)
+    mail.init_app(app)
 
     db.init_app(app)
       # Import models to register them with SQLAlchemy
